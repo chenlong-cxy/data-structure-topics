8a77a498bd45e7580ca453f16bb2e2bc7bc93c94
chenlong_cxy <chenlonghyl@163.com>
2021-04-12 14:39:57 +0800

    增加

--- Linked_mid_node/Linked_mid_node/test.c
+++ Linked_mid_node/Linked_mid_node/test.c
@@ -2,6 +2,30 @@
 //给定一个头结点为 head 的非空单链表，返回链表的中间结点。
 //如果有两个中间结点，则返回第二个中间结点。
 
+//#include <stdio.h>
+//struct ListNode {
+//	int val;
+//	struct ListNode *next;
+//};
+//struct ListNode* middleNode(struct ListNode* head)
+//{
+//	struct ListNode* cur = head;//记录当前结点位置
+//	int count = 0;//记录链表中结点的总数
+//	while (cur)//遍历的停止条件
+//	{
+//		count++;//总数加一
+//		cur = cur->next;//指针后移
+//	}
+//	int mid = count / 2;//中间结点与第一个结点之间相差的结点数
+//	struct ListNode* midnode = head;//记录中间结点的位置
+//	while (mid--)//从第一个结点开始，指针后移mid个结点
+//	{
+//		midnode = midnode->next;//指针后移
+//	}
+//	return midnode;//返回中间结点
+//}
+
+
 #include <stdio.h>
 struct ListNode {
 	int val;
@@ -9,18 +33,12 @@ struct ListNode {
 };
 struct ListNode* middleNode(struct ListNode* head)
 {
-	struct ListNode* cur = head;//记录当前结点位置
-	int count = 0;//记录链表中结点的总数
-	while (cur)//遍历的停止条件
+	struct ListNode* fast = head;//快指针
+	struct ListNode* slow = head;//慢指针
+	while (fast&&fast->next)//遍历继续的条件
 	{
-		count++;//总数加一
-		cur = cur->next;//指针后移
+		slow = slow->next;//慢指针一次走一步
+		fast = fast->next->next;//快指针一次走两步
 	}
-	int mid = count / 2;//中间结点与第一个结点之间相差的结点数
-	struct ListNode* midnode = head;//记录中间结点的位置
-	while (mid--)//从第一个结点开始，指针后移mid个结点
-	{
-		midnode = midnode->next;//指针后移
-	}
-	return midnode;//返回中间结点
+	return slow;//返回慢指针
 }
