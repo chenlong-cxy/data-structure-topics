84e220afd45fbddc9ecc57c7b9063646c21eaea1
chenlong_cxy <chenlonghyl@163.com>
2021-05-08 15:15:48 +0800

    带环链表相关问题

---
+++ Cycle_Problem/Cycle_Problem/test.c
@@ -0,0 +1,48 @@
+//给定一个链表，判断链表中是否有环。
+
+#include <stdio.h>
+#include <stdbool.h>
+struct ListNode {
+	int val;
+	struct ListNode *next;
+};
+bool hasCycle(struct ListNode *head) {
+	struct ListNode* slow = head;
+	struct ListNode* fast = head;
+	while (fast && fast->next)
+	{
+		fast = fast->next->next;//兔子走两步
+		slow = slow->next;//乌龟走一步
+		if (fast == slow)//兔子与乌龟相遇
+			return true;
+	}
+	return false;
+}
+
+//给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+
+#include <stdio.h>
+struct ListNode {
+	int val;
+	struct ListNode *next;
+};
+struct ListNode *detectCycle(struct ListNode *head) {
+	struct ListNode* fast = head;
+	struct ListNode* slow = head;
+	while (fast && fast->next)
+	{
+		slow = slow->next;//慢指针走一步
+		fast = fast->next->next;//快指针走两步
+		if (fast == slow)//相遇
+		{
+			struct ListNode* meet = fast;
+			while (head != meet)
+			{
+				head = head->next;
+				meet = meet->next;
+			}
+			return meet;
+		}
+	}
+	return NULL;
+}
