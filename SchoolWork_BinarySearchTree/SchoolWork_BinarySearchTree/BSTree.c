f2c86968ff15ac52584e0e2aa5e7ffa9d6f381d9
chenlong_cxy <chenlonghyl@163.com>
2021-05-25 21:26:17 +0800

    二叉搜索树

--- SchoolWork_BinarySearchTree/SchoolWork_BinarySearchTree/BSTree.c
+++ SchoolWork_BinarySearchTree/SchoolWork_BinarySearchTree/BSTree.c
@@ -37,10 +37,23 @@ BTNode* Insert(BTNode* root, BTDataType x)
 	}
 }
 //删除
-void Delet(BTNode* root, BTDataType x);
+void Delet(BTNode* root, BTDataType x)
+{
+
+}
 
 //查找（递归）
-BTNode* Find(BTNode* root, BTDataType x);
+BTNode* Find(BTNode* root, BTDataType x)
+{
+	if (root == NULL)
+		return NULL;
+	if (x > root->data)//右子树找
+		return Find(root->right, x);
+	else if (x < root->data)//左子树找
+		return Find(root->left, x);
+	return root;
+}
+
 //查找（非递归）
 BTNode* FindNonR(BTNode* root, BTDataType x);
 
@@ -54,6 +67,30 @@ BTNode* FindMin(BTNode* root);
 //查找最小元素（非递归）
 BTNode* FindMinNonR(BTNode* root);
 
+////层序遍历
+//void BinaryLevelOrder(BTNode* root)
+//{
+//	Queue q;
+//	QueueInit(&q);//初始化队列
+//	if (root != NULL)
+//		QueuePush(&q, root);
+//	while (!QueueEmpty(&q))//当队列不为空时，循环继续
+//	{
+//		BTNode* front = QueueFront(&q);//读取队头元素
+//		QueuePop(&q);//删除队头元素
+//		printf("%d ", front->data);//打印出队的元素
+//		if (front->left)
+//		{
+//			QueuePush(&q, front->left);//出队元素的左孩子入队列
+//		}
+//		if (front->right)
+//		{
+//			QueuePush(&q, front->right);//出队元素的右孩子入队列
+//		}
+//	}
+//	QueueDestroy(&q);//销毁队列
+//}
+
 //层序遍历
 void BinaryLevelOrder(BTNode* root)
 {
@@ -65,13 +102,17 @@ void BinaryLevelOrder(BTNode* root)
 	{
 		BTNode* front = QueueFront(&q);//读取队头元素
 		QueuePop(&q);//删除队头元素
-		printf("%d ", front->data);//打印出队的元素
-		if (front->left)
+		if (front == NULL)
+		{
+			printf("NULL ");
+		}
+		else
+		{
+			printf("%d ", front->data);//打印出队的元素
+		}
+		if (front)
 		{
 			QueuePush(&q, front->left);//出队元素的左孩子入队列
-		}
-		if (front->right)
-		{
 			QueuePush(&q, front->right);//出队元素的右孩子入队列
 		}
 	}
--- SchoolWork_BinarySearchTree/SchoolWork_BinarySearchTree/test.c
+++ SchoolWork_BinarySearchTree/SchoolWork_BinarySearchTree/test.c
@@ -8,6 +8,17 @@ int main()
 	p = Insert(p, 35);
 	p = Insert(p, 4);
 	p = Insert(p, 5);
-	BinaryLevelOrder(p);
+	//BinaryLevelOrder(p);
+	BTNode* find = Find(p, 35);
+	printf("%d\n", find->data);
+	 find = Find(p, 1);
+	if (find == NULL)
+	{
+		printf("²éÕÒÊ§°Ü\n");
+	}
+	else
+	{
+		printf("%d\n", find->data);
+	}
 	return 0;
 }
