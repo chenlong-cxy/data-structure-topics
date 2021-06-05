8f882f00d3ebf497a16ab484c81cab2bc103bcef
chenlong_cxy <chenlonghyl@163.com>
2021-06-05 16:13:36 +0800

    希尔排序

--- SchoolWork_ShellSort/School_ShellSort/test.c
+++ SchoolWork_ShellSort/School_ShellSort/test.c
@@ -50,11 +50,34 @@ void ShellSort(int* arr, int n)
 		}
 	}
 }
+// Ï£¶ûÅÅÐò£¨Êé£©
+void ShellSortBook(int* arr, int n)
+{
+	int Sedgewick[] = { 929, 505, 209, 109, 41, 19, 5, 1, 0 };
+	int Si = 0;
+	for (Si = 0; Sedgewick[Si] >= n; Si++)
+		;
+	int D = 0;
+	for (D = Sedgewick[Si]; D > 0; D = Sedgewick[++Si])
+	{
+		int P = 0;
+		for (P = D; P < n; P++)
+		{
+			int tmp = arr[P];
+			int i = 0;
+			for (i = P; i>=D&&arr[i - D]>tmp; i -= D)
+			{
+				arr[i] = arr[i - D];
+			}
+			arr[i] = tmp;
+		}
+	}
+}
 int main()
 {
 	int arr[] = { 24, 76, 23, 55, 89, 65, 18, 5, 0, 25, 34, 67, 12 };
 	int n = sizeof(arr) / sizeof(arr[0]);
-	ShellSort(arr, n);
+	ShellSortBook(arr, n);
 
 	int i = 0;
 	for (i = 0; i < n; i++)
