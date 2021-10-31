c07f68436c19df28621525e1b4c9618e376ede8a
chenlong-xcy <chenlonghyl@163.com>
2021-10-31 22:01:37 +0800

    二叉搜索树

--- BSTree/BSTree/BSTree.h
+++ BSTree/BSTree/BSTree.h
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
+
 template<class K>
 struct BSTreeNode
 {
 	K _key;
 	BSTreeNode<K>* _left;
 	BSTreeNode<K>* _right;
-	BSTreeNode(const K& key)
+
+	BSTreeNode(const K& key = 0)
 		:_key(key)
 		, _left(nullptr)
 		, _right(nullptr)
@@ -18,7 +20,12 @@ class BSTree
 {
 	typedef BSTreeNode<K> Node;
 public:
-	//非递归
+	//构造函数
+	BSTree()
+		:_root(nullptr)
+	{}
+
+	//插入函数
 	bool Insert(const K& key)
 	{
 		if (_root == nullptr)
@@ -30,15 +37,15 @@ public:
 		Node* cur = _root;
 		while (cur)
 		{
-			if (key > cur->_key)
-			{
-				parent = cur;
-				cur = cur->_right;
-			}
-			else if (key < cur->_key)
+			if (key < cur->_key)
 			{
 				parent = cur;
 				cur = cur->_left;
+			}
+			else if (key > cur->_key)
+			{
+				parent = cur;
+				cur = cur->_right;
 			}
 			else
 			{
@@ -46,7 +53,7 @@ public:
 			}
 		}
 		cur = new Node(key);
-		if (cur = parent->_left)
+		if (key < parent->_key)
 		{
 			parent->_left = cur;
 		}
@@ -56,49 +63,371 @@ public:
 		}
 		return true;
 	}
+	//递归插入函数的子函数
+	bool _InsertR(Node*& root, const K& key) //注意引用
+	{
+		if (root == nullptr)
+		{
+			root = new Node(key);
+			return true;
+		}
+
+		if (key < root->_key)
+			return _Insert(root->_left, key);
+		else if (key > root->_key)
+			return _Insert(root->_right, key);
+		else
+			return false;
+	}
+	//递归插入函数
+	bool InsertR(const K& key)
+	{
+		return _Insert(_root, key);
+	}
+	//删除函数1
+	//bool Erase(const K& key)
+	//{
+	//	Node* parent = nullptr;
+	//	Node* cur = _root;
+	//	while (cur)
+	//	{
+	//		if (key < cur->_key)
+	//		{
+	//			parent = cur;
+	//			cur = cur->_left;
+	//		}
+	//		else if (key > cur->_key)
+	//		{
+	//			parent = cur;
+	//			cur = cur->_right;
+	//		}
+	//		else
+	//		{
+	//			//找到了
+	//			if (cur->_left == nullptr)
+	//			{
+	//				if (cur == _root)
+	//				{
+	//					_root = cur->_right;
+	//				}
+	//				else
+	//				{
+	//					if (parent->_left == cur)
+	//					{
+	//						parent->_left = cur->_right;
+	//					}
+	//					else
+	//					{
+	//						parent->_right = cur->_right;
+	//					}
+	//				}
+	//				delete cur;
+	//				return true;
+	//			}
+	//			else if (cur->_right == nullptr)
+	//			{
+	//				if (cur == _root)
+	//				{
+	//					_root = cur->_left;
+	//				}
+	//				else
+	//				{
+	//					if (parent->_left = cur)
+	//					{
+	//						parent->_left = cur->_left;
+	//					}
+	//					else
+	//					{
+	//						parent->_right = cur->_left;
+	//					}
+	//				}
+	//				delete cur;
+	//				return true;
+	//			}
+	//			else
+	//			{
+	//				Node* minParent = cur;
+	//				Node* minRight = cur->_right;
+	//				while (minRight->_left)
+	//				{
+	//					minParent = minRight;
+	//					minRight = minRight->_left;
+	//				}
+	//				cur->_key = minRight->_key;
+	//				if (minParent->_left == minRight)
+	//				{
+	//					minParent->_left = minRight->_right;
+	//				}
+	//				else
+	//				{
+	//					minParent->_right = minRight->_right;
+	//				}
+
+	//				delete minRight;
+	//				return true;
+	//			}
+	//		}
+	//	}
+	//	return false;
+	//}
+
+	//递归删除函数1
+	bool EraseR(const K& key)
+	{
+		 
+	}
+
+	//删除函数2
 	bool Erase(const K& key)
 	{
 		Node* parent = nullptr;
 		Node* cur = _root;
 		while (cur)
 		{
-			if (key > cur->_key)
-			{
-				parent = cur;
-				cur = cur->_right;
-			}
-			else if (key < cur->_key)
+			if (key < cur->_key)
 			{
 				parent = cur;
 				cur = cur->_left;
 			}
+			else if (key > cur->_key)
+			{
+				parent = cur;
+				cur = cur->_right;
+			}
 			else
 			{
-
+				//找到了
+				if (cur->_left == nullptr)
+				{
+					if (cur == _root)
+					{
+						_root = cur->_right;
+					}
+					else
+					{
+						if (parent->_left == cur)
+						{
+							parent->_left = cur->_right;
+						}
+						else
+						{
+							parent->_right = cur->_right;
+						}
+					}
+					delete cur;
+					return true;
+				}
+				else if (cur->_right == nullptr)
+				{
+					if (cur == _root)
+					{
+						_root = cur->_left;
+					}
+					else
+					{
+						if (parent->_left = cur)
+						{
+							parent->_left = cur->_left;
+						}
+						else
+						{
+							parent->_right = cur->_left;
+						}
+					}
+					delete cur;
+					return true;
+				}
+				else
+				{
+					Node* minRight = cur->_right;
+					while (minRight->_left)
+					{
+						minRight = minRight->_left;
+					}
+					K minKey = minRight->_key;
+					Erase(minKey); //this->Erase(minKey);
+					cur->_key = minKey;
+				}
 			}
 		}
+		return false;
 	}
+
+	//查找函数
 	Node* Find(const K& key)
 	{
 		Node* cur = _root;
 		while (cur)
 		{
-			if (key > cur->_key)
-			{
-				cur = cur->_right;
-			}
-			else if (key < cur->_key)
+			if (key < cur->_key)
 			{
 				cur = cur->_left;
 			}
+			else if (key > cur->_key)
+			{
+				cur = cur->_right;
+			}
 			else
 			{
+				//找到了
 				return cur;
 			}
 		}
 		return nullptr;
 	}
+	//递归查找函数的子函数
+	Node* _FindR(Node* root, const K& key)
+	{
+		if (root == nullptr)
+			return nullptr;
+
+		if (key < root->_key)
+			return _FindR(root->_left, key);
+		else if (key > root->_key)
+			return _FindR(root->_right, key);
+		else
+			return root;
+	}
+	//递归查找函数
+	Node* FindR(const K& key)
+	{
+		return _FindR(_root, key);
+	}
+	//中序遍历的子函数
+	void _InOrder(Node* root)
+	{
+		if (root == nullptr)
+			return;
+		_InOrder(root->_left);
+		cout << root->_key << " ";
+		_InOrder(root->_right);
+	}
+	//中序遍历
+	void InOrder()
+	{
+		_InOrder(_root);
+		cout << endl;
+	}
 private:
 	Node* _root;
 };
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//#include <iostream>
+//using namespace std;
+//template<class K>
+//struct BSTreeNode
+//{
+//	K _key;
+//	BSTreeNode<K>* _left;
+//	BSTreeNode<K>* _right;
+//	BSTreeNode(const K& key)
+//		:_key(key)
+//		, _left(nullptr)
+//		, _right(nullptr)
+//	{}
+//};
+//
+//template<class K>
+//class BSTree
+//{
+//	typedef BSTreeNode<K> Node;
+//public:
+//	//非递归
+//	bool Insert(const K& key)
+//	{
+//		if (_root == nullptr)
+//		{
+//			_root = new Node(key);
+//			return true;
+//		}
+//		Node* parent = nullptr;
+//		Node* cur = _root;
+//		while (cur)
+//		{
+//			if (key > cur->_key)
+//			{
+//				parent = cur;
+//				cur = cur->_right;
+//			}
+//			else if (key < cur->_key)
+//			{
+//				parent = cur;
+//				cur = cur->_left;
+//			}
+//			else
+//			{
+//				return false;
+//			}
+//		}
+//		cur = new Node(key);
+//		if (cur = parent->_left) //error
+//		{
+//			parent->_left = cur;
+//		}
+//		else
+//		{
+//			parent->_right = cur;
+//		}
+//		return true;
+//	}
+//	bool Erase(const K& key)
+//	{
+//		Node* parent = nullptr;
+//		Node* cur = _root;
+//		while (cur)
+//		{
+//			if (key > cur->_key)
+//			{
+//				parent = cur;
+//				cur = cur->_right;
+//			}
+//			else if (key < cur->_key)
+//			{
+//				parent = cur;
+//				cur = cur->_left;
+//			}
+//			else
+//			{
+//
+//			}
+//		}
+//	}
+//	Node* Find(const K& key)
+//	{
+//		Node* cur = _root;
+//		while (cur)
+//		{
+//			if (key > cur->_key)
+//			{
+//				cur = cur->_right;
+//			}
+//			else if (key < cur->_key)
+//			{
+//				cur = cur->_left;
+//			}
+//			else
+//			{
+//				return cur;
+//			}
+//		}
+//		return nullptr;
+//	}
+//private:
+//	Node* _root;
+//};
--- BSTree/BSTree/test.cpp
+++ BSTree/BSTree/test.cpp
@@ -1,3 +1,54 @@
+#include "BSTree.h"
+
+int main()
+{
+	BSTree<int> t;
+	int arr[] = { 5, 3, 4, 1, 7, 8, 2, 6, 0, 9 };
+	for (auto e : arr)
+	{
+		t.Insert(e);
+	}
+	t.InOrder();
+	t.Erase(5);
+	t.InOrder();
+
+	t.Erase(3);
+	t.InOrder();
+
+	t.Erase(4);
+	t.InOrder();
+
+	t.Erase(1);
+	t.InOrder();
+
+	t.Erase(7);
+	t.InOrder();
+
+	t.Erase(8);
+	t.InOrder();
+
+	t.Erase(2);
+	t.InOrder();
+
+	t.Erase(6);
+	t.InOrder();
+
+	t.Erase(9);
+	t.InOrder();
+
+	t.Erase(0);
+	t.InOrder();
+	//for (auto e : arr)
+	//{
+	//	t.Erase(e);
+	//	t.InOrder();
+	//}
+
+	return 0;
+}
+
+
+
 //bool EraseR(Node*& root, const K& key)
 //{
 //	if (root == nullptr)
@@ -35,18 +86,18 @@
 //	}
 //}
 
-bool FindPath(TreeNode* root, TreeNode* x, stack<TreeNode*>& path)
-{
-	if (root == nullptr)
-		return false;
-	if (root == x)
-		return true;
-	path.push(root);
-	bool leftfind = FindPath(root->left, x, path);
-	if (leftfind)
-		return true;
-	bool rightfind = FindPath(root->right, x, path);
-	if (rightfind)
-		return true;
-	return true;
-}
+//bool FindPath(TreeNode* root, TreeNode* x, stack<TreeNode*>& path)
+//{
+//	if (root == nullptr)
+//		return false;
+//	if (root == x)
+//		return true;
+//	path.push(root);
+//	bool leftfind = FindPath(root->left, x, path);
+//	if (leftfind)
+//		return true;
+//	bool rightfind = FindPath(root->right, x, path);
+//	if (rightfind)
+//		return true;
+//	return true;
+//}
