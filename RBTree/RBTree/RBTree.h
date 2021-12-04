23d21f2bf0af4eec9c2bcc2a345674979634c021
chenlong-xcy <chenlonghyl@163.com>
2021-12-04 23:07:52 +0800

    红黑树的实现

--- RBTree/RBTree/RBTree.h
+++ RBTree/RBTree/RBTree.h
@@ -34,6 +34,22 @@ struct RBTreeNode
 };
 
 template<class K, class V>
+struct __TreeIterator
+{
+	typedef RBTreeNode<K, V> Node;
+
+	Node* _node;
+
+	__TreeIterator(Node* node)
+		:_node(node)
+	{}
+
+	//operator*();
+	//operator++();
+	//operator--();
+};
+
+template<class K, class V>
 class RBTree
 {
 	typedef RBTreeNode<K, V> Node;
@@ -75,35 +91,128 @@ public:
 	}
 
 	//插入函数
-	bool Insert(const pair<K, V>& kv)
+	pair<Node*, bool> Insert(const pair<K, V>& kv)
 	{
-		if (_root == nullptr)
+		if (_root == nullptr) //若红黑树为空树，则插入结点直接作为根结点
 		{
 			_root = new Node(kv);
-			return true;
+			_root->_col = BLACK; //根结点必须是黑色
+			return make_pair(_root, true); //插入成功
 		}
+		//1、按二叉搜索树的插入方法，找到待插入位置
 		Node* cur = _root;
 		Node* parent = nullptr;
 		while (cur)
 		{
-			if (kv.first < cur->_kv.first)
+			if (kv.first < cur->_kv.first) //待插入结点的key值小于当前结点的key值
 			{
+				//往该结点的左子树走
 				parent = cur;
 				cur = cur->_left;
 			}
-			else if (kv.first > cur->_kv.first)
+			else if (kv.first > cur->_kv.first) //待插入结点的key值大于当前结点的key值
 			{
+				//往该结点的右子树走
 				parent = cur;
 				cur = cur->_right;
 			}
-			else
+			else //待插入结点的key值等于当前结点的key值
 			{
-				return false; //插入失败
+				return make_pair(cur, false); //插入失败
 			}
 		}
-		//进行插入
-		Node* newnode = new Node(kv);
-		return false; /////////////////////
+
+		//2、将待插入结点插入到树中
+		cur = new Node(kv); //根据所给值构造一个结点
+		Node* newnode = cur; //记录新插入的结点（便于后序返回）
+		if (kv.first < parent->_kv.first) //新结点的key值小于parent的key值
+		{
+			//插入到parent的左边
+			parent->_left = cur;
+			cur->_parent = parent;
+		}
+		else //新结点的key值大于parent的key值
+		{
+			//插入到parent的右边
+			parent->_right = cur;
+			cur->_parent = parent;
+		}
+
+		//3、若插入结点的父结点是红色的，则需要对红黑树进行调整
+		while (parent&&parent->_col == RED)
+		{
+			Node* grandfather = parent->_parent; //parent是红色，则其父结点一定存在
+			if (parent == grandfather->_left) //parent是grandfather的左孩子
+			{
+				Node* uncle = grandfather->_right; //uncle是grandfather的右孩子
+				if (uncle&&uncle->_col == RED) //情况1：uncle存在且为红
+				{
+					//颜色调整
+					parent->_col = uncle->_col = BLACK;
+					grandfather->_col = RED;
+
+					//继续往上处理
+					cur = grandfather;
+					parent = cur->_parent;
+				}
+				else //情况2+情况3：uncle不存在 + uncle存在且为黑
+				{
+					if (cur == parent->_left)
+					{
+						RotateR(grandfather); //右单旋
+
+						//颜色调整
+						grandfather->_col = RED;
+						parent->_col = BLACK;
+					}
+					else //cur == parent->_right
+					{
+						RotateLR(grandfather); //左右双旋
+
+						//颜色调整
+						grandfather->_col = RED;
+						cur->_col = BLACK;
+					}
+					break; //子树旋转后，该子树的根变成了黑色，无需继续往上进行处理
+				}
+			}
+			else //parent是grandfather的右孩子
+			{
+				Node* uncle = grandfather->_left; //uncle是grandfather的左孩子
+				if (uncle&&uncle->_col == RED) //情况1：uncle存在且为红
+				{
+					//颜色调整
+					uncle->_col = parent->_col = BLACK;
+					grandfather->_col = RED;
+
+					//继续往上处理
+					cur = grandfather;
+					parent = cur->_parent;
+				}
+				else //情况2+情况3：uncle不存在 + uncle存在且为黑
+				{
+					if (cur == parent->_left)
+					{
+						RotateRL(grandfather); //右左双旋
+
+						//颜色调整
+						cur->_col = BLACK;
+						grandfather->_col = RED;
+					}
+					else //cur == parent->_right
+					{
+						RotateL(grandfather); //左单旋
+
+						//颜色调整
+						grandfather->_col = RED;
+						parent->_col = BLACK;
+					}
+					break; //子树旋转后，该子树的根变成了黑色，无需继续往上进行处理
+				}
+			}
+		}
+		_root->_col = BLACK; //根结点的颜色为黑色（可能被情况一变成了红色，需要变回黑色）
+		return make_pair(newnode, true); //插入成功
 	}
 
 	//删除函数
@@ -131,7 +240,54 @@ public:
 		}
 		return false; ////////////////
 	}
+	
+	//判断是否为红黑树
+	bool ISRBTree()
+	{
+		if (_root->_col == RED)
+		{
+			cout << "error：根结点为红色" << endl;
+			return false;
+		}
+		
+		//任意计算一条从根到叶子的路径当中黑色结点的数目
+		Node* cur = _root;
+		int BlackCount = 0;
+		while (cur)
+		{
+			if (cur->_col == BLACK)
+				BlackCount++;
+			cur = cur->_left;
+		}
+
+		int count = 0;
+		return _ISRBTree(_root, count, BlackCount);
+	}
 private:
+	//判断是否为红黑树的子函数
+	bool _ISRBTree(Node* root, int count, int BlackCount)
+	{
+		//该路径已经走完了
+		if (root == nullptr)
+		{
+			if (count != BlackCount)
+			{
+				cout << "error：从根到叶子的路径所包含黑色结点的数目不相同" << endl;
+				return false;
+			}
+			return true;
+		}
+		if (root->_col == BLACK)
+			count++;
+		Node* parent = root->_parent;
+		if (parent&&parent->_col == RED&&root->_col == RED)
+		{
+			cout << "error：存在连续的红色结点" << endl;
+			return false;
+		}
+		return _ISRBTree(root->_left, count, BlackCount);
+	}
+	//析构函数子函数
 	void _Destroy(Node* root)
 	{
 		if (root == nullptr)
@@ -142,11 +298,12 @@ private:
 		_Destroy(root->_right);
 		delete root;
 	}
+
 	//左单旋
 	void RotateL(Node* parent)
 	{
 		Node* subR = parent->_right;
-		Node* subRL = subL->_left;
+		Node* subRL = subR->_left;
 		Node* parentParent = parent->_parent;
 
 		//建立subRL与parent之间的联系
--- RBTree/RBTree/test.cpp
+++ RBTree/RBTree/test.cpp
@@ -1,7 +1,17 @@
 #include "RBTree.h"
-
+void test1()
+{
+	RBTree<int, int> t;
+	//int arr[] = { 16, 3, 7, 11, 9, 26, 18, 14, 15 };
+	int arr[] = { 4, 2, 6, 1, 3, 5, 15, 7, 16, 14 };
+	for (auto e : arr)
+	{
+		t.Insert(make_pair(e, e));
+	}
+	cout << t.ISRBTree() << endl;
+}
 int main()
 {
-
+	test1();
 	return 0;
 }
