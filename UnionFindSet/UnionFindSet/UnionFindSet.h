e994b8fa976e395a7664d8d6324123263b91247f
chenlong-xcy <chenlonghyl@163.com>
2023-04-28 15:59:17 +0800

    并查集实现与应用

--- UnionFindSet/UnionFindSet/UnionFindSet.h
+++ UnionFindSet/UnionFindSet/UnionFindSet.h
@@ -5,51 +5,103 @@
 #include <unordered_map>
 using namespace std;
 
+////并查集
+//class UnionFindSet {
+//public:
+//	//构造函数
+//	UnionFindSet(int n)
+//		:_ufs(n, -1) //初始时各个元素自成一个集合
+//	{}
+//	//查找元素所在集合的根结点（迭代）
+//	int findRoot(int x) {
+//		int parent = x; //默认当前结点就是根结点
+//		while (_ufs[parent] >= 0) { //当前结点值不是负数则继续向上找
+//			parent = _ufs[parent];
+//		}
+//		return parent; //返回根结点
+//	}
+//	////查找元素所在集合的根结点（递归）
+//	//int findRoot(int x) {
+//	//	return _ufs[x] < 0 ? x : findRoot(_ufs[x]);
+//	//}
+//	//查找元素所在集合的根结点（递归）
+//	int findRoot(int x) {
+//		int parent = x; //默认当前结点就是根结点
+//		if (_ufs[x] >= 0) { //当前结点值不是负数则继续向上找
+//			parent = findRoot(_ufs[x]); //找到根结点
+//			_ufs[x] = parent; //将当前结点的父亲改为根结点（路径压缩）
+//		}
+//		return parent; //返回根结点
+//	}
+//	////递归+压缩
+//	//int findRoot(int x) {
+//	//	if (_ufs[x] >= 0) {
+//	//		_ufs[x] = findRoot(_ufs[x]);
+//	//		return _ufs[x];
+//	//	}
+//	//	return x;
+//	//}
+//	//合并两个元素所在的集合
+//	bool unionSet(int x1, int x2) {
+//		int parent1 = findRoot(x1), parent2 = findRoot(x2); //分别找到两个元素所在集合的根结点
+//		if (parent1 == parent2) //两个结点已经位于同一集合
+//			return false;
+//		if (_ufs[parent1] > _ufs[parent2]) //让parent1标记大集合根结点，parent2标记小集合根结点
+//			swap(parent1, parent2);
+//		//将小集合合并到大集合上
+//		_ufs[parent1] += _ufs[parent2]; //将小集合根结点的值累加到大集合的根结点上
+//		_ufs[parent2] = parent1; //将小集合根结点的值改为大集合根结点的编号
+//		return true;
+//	}
+//	//获取并查集中集合的个数
+//	int getNum() {
+//		int count = 0; //统计根结点的个数
+//		for (const int& val : _ufs) {
+//			if (val < 0) //元素值为负数则为根结点
+//				count++;
+//		}
+//		return count; //返回根结点的个数
+//	}
+//	void print() {
+//		for (const auto& e : _ufs)
+//			cout << e << " ";
+//		cout << endl;
+//	}
+//private:
+//	vector<int> _ufs; //维护各个结点之间的关系
+//};
+
 //并查集
+template<class T>
 class UnionFindSet {
 public:
-	UnionFindSet(int n)
-		:_ufs(n, -1) //初始时各个元素自成一个集合
-	{}
-	//找到元素所在集合的根结点（迭代）
-	int findRoot(int x) {
-		int parent = x; //默认当前结点就是根结点
+	//构造函数
+	UnionFindSet(const vector<T>& v)
+		:_ufs(v.size(), -1) //初始时各个元素自成一个集合
+	{
+		//建立元素与数组下标之间的映射关系
+		for (int i = 0; i < v.size(); i++) {
+			_indexMap[v[i]] = i;
+		}
+	}
+	//查找元素所在集合的根结点（迭代）
+	int findRoot(const T& x) {
+		int parent = _indexMap[x]; //默认当前结点就是根结点
 		while (_ufs[parent] >= 0) { //当前结点值不是负数则继续向上找
 			parent = _ufs[parent];
 		}
 		return parent; //返回根结点
 	}
-	////递归
-	//int findRoot(int x) {
-	//	return _ufs[x] < 0 ? x : findRoot(_ufs[x]);
-	//}
-	////递归+压缩
-	//int findRoot(int x) {
-	//	int parent = x;
-	//	if (_ufs[x] >= 0) {
-	//		parent = findRoot(_ufs[x]);
-	//		_ufs[x] = parent;
-	//	}
-	//	return parent;
-	//}
-	////递归+压缩
-	//int findRoot(int x) {
-	//	if (_ufs[x] >= 0) {
-	//		_ufs[x] = findRoot(_ufs[x]);
-	//		return _ufs[x];
-	//	}
-	//	return x;
-	//}
 	//合并两个元素所在的集合
-	bool unionSet(int x1, int x2) {
+	bool unionSet(const T& x1, const T& x2) {
 		int parent1 = findRoot(x1), parent2 = findRoot(x2); //分别找到两个元素所在集合的根结点
 		if (parent1 == parent2) //两个结点已经位于同一集合
 			return false;
-		if (_ufs[parent1] > _ufs[parent2]) //把小集合合并到大集合上
+		if (_ufs[parent1] > _ufs[parent2]) //让parent1标记大集合根结点，parent2标记小集合根结点
 			swap(parent1, parent2);
-		//将x2所在的集合合并到x1所在的集合
-		_ufs[parent1] += _ufs[parent2]; //更新x1所在集合的元素个数
-		_ufs[parent2] = parent1; //让x1所在集合的根结点作为x2所在集合的根结点的父亲
+		//将小集合合并到大集合上
+		_ufs[parent1] += _ufs[parent2]; //将小集合根结点的值累加到大集合的根结点上
+		_ufs[parent2] = parent1; //将小集合根结点的值改为大集合根结点的编号
 		return true;
 	}
 	//获取并查集中集合的个数
@@ -68,41 +120,75 @@ public:
 	}
 private:
 	vector<int> _ufs; //维护各个结点之间的关系
+	unordered_map<T, int> _indexMap; //建立元素与数组下标之间的映射关系
 };
 
-//template<class T>
-//class UnionFindSet {
+////省份数量
+//class Solution {
 //public:
-//	UnionFindSet(vector<T> v)
-//		:_ufs(v.size(), -1) {
-//		for (int i = 0; i < v.size(); i++) {
-//			_indexMap[v[i]] = i;
+//	int findCircleNum(vector<vector<int>>& isConnected) {
+//		//1、初始化并查集
+//		int n = isConnected.size();
+//		vector<int> ufs(n, -1);
+//		auto findRoot = [&ufs](int x)->int {
+//			int parent = x;
+//			while (ufs[parent] >= 0) {
+//				parent = ufs[parent];
+//			}
+//			return parent;
+//		};
+//		//2、根据所给矩阵合并集合
+//		for (int i = 0; i < n; i++) {
+//			for (int j = 0; j < n; j++) {
+//				if (isConnected[i][j] == 1) { //城市相连
+//					int parent1 = findRoot(i), parent2 = findRoot(j);
+//					if (parent1 != parent2) { //需要合并
+//						ufs[parent2] = parent1;
+//					}
+//				}
+//			}
 //		}
-//	}
-//	int findRoot(const T& x) {
-//		int parent = _indexMap[x];
-//		while (_ufs[parent] >= 0) {
-//			parent = _ufs[parent];
-//		}
-//		return parent;
-//	}
-//	bool unionSet(const T& x1, const T& x2) {
-//		int parent1 = findRoot(x1), parent2 = findRoot(x2);
-//		if (parent1 == parent2)
-//			return false;
-//		_ufs[parent1] += _ufs[parent2];
-//		_ufs[parent2] = parent1;
-//		return true;
-//	}
-//	int getNum() {
+//		//3、统计并查集中集合的个数
 //		int count = 0;
-//		for (const int& e : _ufs) {
+//		for (const int& e : ufs) {
 //			if (e < 0)
 //				count++;
 //		}
 //		return count;
 //	}
-//private:
-//	vector<int> _ufs;
-//	unordered_map<T, int> _indexMap;
+//};
+
+////等式方程的可满足性
+//class Solution {
+//public:
+//	bool equationsPossible(vector<string>& equations) {
+//		//1、初始化并查集
+//		vector<int> ufs(26, -1);
+//		auto findRoot = [&ufs](int x)->int {
+//			int parent = x;
+//			while (ufs[parent] >= 0) {
+//				parent = ufs[parent];
+//			}
+//			return parent;
+//		};
+//		//2、根据字符串方程组中的等式合并集合
+//		for (const string& s : equations) {
+//			if (s[1] == '=') { //等式
+//				int parent1 = findRoot(s[0] - 'a'), parent2 = findRoot(s[3] - 'a');
+//				if (parent1 != parent2) { //需要合并
+//					ufs[parent2] = parent1;
+//				}
+//			}
+//		}
+//		//3、根据并查集验证字符串方程组中的不等式
+//		for (const string& s : equations) {
+//			if (s[1] == '!') { //不等式
+//				int parent1 = findRoot(s[0] - 'a'), parent2 = findRoot(s[3] - 'a');
+//				if (parent1 == parent2) { //在同一个集合
+//					return false;
+//				}
+//			}
+//		}
+//		return true;
+//	}
 //};
--- UnionFindSet/UnionFindSet/test.cpp
+++ UnionFindSet/UnionFindSet/test.cpp
@@ -1,35 +1,35 @@
 #include "UnionFindSet.h"
 
 int main() {
-	UnionFindSet ufs(10);
-	ufs.unionSet(0, 6);
-	ufs.unionSet(0, 7);
-	ufs.unionSet(0, 8);
+	//UnionFindSet ufs(10);
+	//ufs.unionSet(0, 6);
+	//ufs.unionSet(0, 7);
+	//ufs.unionSet(0, 8);
 
-	ufs.unionSet(1, 4);
-	ufs.unionSet(1, 9);
+	//ufs.unionSet(1, 4);
+	//ufs.unionSet(1, 9);
 
-	ufs.unionSet(2, 3);
-	ufs.unionSet(2, 5);
+	//ufs.unionSet(2, 3);
+	//ufs.unionSet(2, 5);
 
-	//ufs.unionSet(8, 1);
-	ufs.unionSet(1, 8);
+	////ufs.unionSet(8, 1);
+	//ufs.unionSet(1, 8);
 
-	//ufs.unionSet(2, 1);
-	ufs.print();
-	//ufs.findRoot(4);
+	////ufs.unionSet(2, 1);
 	//ufs.print();
-	//ufs.findRoot(9);
-	//ufs.print();
+	////ufs.findRoot(4);
+	////ufs.print();
+	////ufs.findRoot(9);
+	////ufs.print();
 
-	//vector<string> v = { "张三", "李四", "王五", "赵六", "田七", "周八", "吴九" };
-	//UnionFindSet<string> ufs(v);
-	//cout << ufs.getNum() << endl;
-	//ufs.unionSet("张三", "李四");
-	//ufs.unionSet("王五", "赵六");
-	//cout << ufs.getNum() << endl;
-	//ufs.unionSet("张三", "赵六");
-	//cout << ufs.getNum() << endl;
+	vector<string> v = { "张三", "李四", "王五", "赵六", "田七", "周八", "吴九" };
+	UnionFindSet<string> ufs(v);
+	cout << ufs.getNum() << endl;
+	ufs.unionSet("张三", "李四");
+	ufs.unionSet("王五", "赵六");
+	cout << ufs.getNum() << endl;
+	ufs.unionSet("张三", "赵六");
+	cout << ufs.getNum() << endl;
 
 	return 0;
 }
