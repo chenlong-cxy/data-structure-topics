3e2f409b7b6135af68ccd82718c8d95f0fc43695
chenlong-xcy <chenlonghyl@163.com>
2023-04-29 20:32:32 +0800

    并查集路径压缩

--- UnionFindSet/UnionFindSet/UnionFindSet.h
+++ UnionFindSet/UnionFindSet/UnionFindSet.h
@@ -5,95 +5,57 @@
 #include <unordered_map>
 using namespace std;
 
-////并查集
-//class UnionFindSet {
-//public:
-//	//构造函数
-//	UnionFindSet(int n)
-//		:_ufs(n, -1) //初始时各个元素自成一个集合
-//	{}
-//	//查找元素所在集合的根结点（迭代）
-//	int findRoot(int x) {
-//		int parent = x; //默认当前结点就是根结点
-//		while (_ufs[parent] >= 0) { //当前结点值不是负数则继续向上找
-//			parent = _ufs[parent];
-//		}
-//		return parent; //返回根结点
-//	}
-//	////查找元素所在集合的根结点（递归）
-//	//int findRoot(int x) {
-//	//	return _ufs[x] < 0 ? x : findRoot(_ufs[x]);
-//	//}
-//	//查找元素所在集合的根结点（递归）
-//	int findRoot(int x) {
-//		int parent = x; //默认当前结点就是根结点
-//		if (_ufs[x] >= 0) { //当前结点值不是负数则继续向上找
-//			parent = findRoot(_ufs[x]); //找到根结点
-//			_ufs[x] = parent; //将当前结点的父亲改为根结点（路径压缩）
-//		}
-//		return parent; //返回根结点
-//	}
-//	////递归+压缩
-//	//int findRoot(int x) {
-//	//	if (_ufs[x] >= 0) {
-//	//		_ufs[x] = findRoot(_ufs[x]);
-//	//		return _ufs[x];
-//	//	}
-//	//	return x;
-//	//}
-//	//合并两个元素所在的集合
-//	bool unionSet(int x1, int x2) {
-//		int parent1 = findRoot(x1), parent2 = findRoot(x2); //分别找到两个元素所在集合的根结点
-//		if (parent1 == parent2) //两个结点已经位于同一集合
-//			return false;
-//		if (_ufs[parent1] > _ufs[parent2]) //让parent1标记大集合根结点，parent2标记小集合根结点
-//			swap(parent1, parent2);
-//		//将小集合合并到大集合上
-//		_ufs[parent1] += _ufs[parent2]; //将小集合根结点的值累加到大集合的根结点上
-//		_ufs[parent2] = parent1; //将小集合根结点的值改为大集合根结点的编号
-//		return true;
-//	}
-//	//获取并查集中集合的个数
-//	int getNum() {
-//		int count = 0; //统计根结点的个数
-//		for (const int& val : _ufs) {
-//			if (val < 0) //元素值为负数则为根结点
-//				count++;
-//		}
-//		return count; //返回根结点的个数
-//	}
-//	void print() {
-//		for (const auto& e : _ufs)
-//			cout << e << " ";
-//		cout << endl;
-//	}
-//private:
-//	vector<int> _ufs; //维护各个结点之间的关系
-//};
-
 //并查集
-template<class T>
 class UnionFindSet {
 public:
 	//构造函数
-	UnionFindSet(const vector<T>& v)
-		:_ufs(v.size(), -1) //初始时各个元素自成一个集合
-	{
-		//建立元素与数组下标之间的映射关系
-		for (int i = 0; i < v.size(); i++) {
-			_indexMap[v[i]] = i;
+	UnionFindSet(int n)
+		:_ufs(n, -1) //初始时各个元素自成一个集合
+	{}
+	////查找元素所在集合的根结点（迭代）
+	//int findRoot(int x) {
+	//	int parent = x; //默认当前结点就是根结点
+	//	while (_ufs[parent] >= 0) { //当前结点值不是负数则继续向上找
+	//		parent = _ufs[parent];
+	//	}
+	//	return parent; //返回根结点
+	//}
+	////查找元素所在集合的根结点（递归）
+	//int findRoot(int x) {
+	//	return _ufs[x] < 0 ? x : findRoot(_ufs[x]);
+	//}
+	////查找元素所在集合的根结点（递归）
+	//int findRoot(int x) {
+	//	int parent = x; //默认当前结点就是根结点
+	//	if (_ufs[x] >= 0) { //当前结点值不是负数则继续向上找
+	//		parent = findRoot(_ufs[x]); //找到根结点
+	//		_ufs[x] = parent; //将当前结点的父亲改为根结点（路径压缩）
+	//	}
+	//	return parent; //返回根结点
+	//}
+	////递归+压缩
+	//int findRoot(int x) {
+	//	if (_ufs[x] >= 0) {
+	//		_ufs[x] = findRoot(_ufs[x]);
+	//		return _ufs[x];
+	//	}
+	//	return x;
+	//}
+	//迭代+压缩
+	int findRoot(int x) {
+		int root = x; //默认当前结点就是根结点
+		while (_ufs[root] >= 0) { //当前结点值不是负数则继续向上找
+			root = _ufs[root];
 		}
-	}
-	//查找元素所在集合的根结点（迭代）
-	int findRoot(const T& x) {
-		int parent = _indexMap[x]; //默认当前结点就是根结点
-		while (_ufs[parent] >= 0) { //当前结点值不是负数则继续向上找
-			parent = _ufs[parent];
+		while (_ufs[x] >= 0) {
+			int tmp = _ufs[x];
+			_ufs[x] = root;
+			x = tmp;
 		}
-		return parent; //返回根结点
+		return root; //返回根结点
 	}
 	//合并两个元素所在的集合
-	bool unionSet(const T& x1, const T& x2) {
+	bool unionSet(int x1, int x2) {
 		int parent1 = findRoot(x1), parent2 = findRoot(x2); //分别找到两个元素所在集合的根结点
 		if (parent1 == parent2) //两个结点已经位于同一集合
 			return false;
@@ -120,8 +82,59 @@ public:
 	}
 private:
 	vector<int> _ufs; //维护各个结点之间的关系
-	unordered_map<T, int> _indexMap; //建立元素与数组下标之间的映射关系
 };
+
+////并查集
+//template<class T>
+//class UnionFindSet {
+//public:
+//	//构造函数
+//	UnionFindSet(const vector<T>& v)
+//		:_ufs(v.size(), -1) //初始时各个元素自成一个集合
+//	{
+//		//建立元素与数组下标之间的映射关系
+//		for (int i = 0; i < v.size(); i++) {
+//			_indexMap[v[i]] = i;
+//		}
+//	}
+//	//查找元素所在集合的根结点（迭代）
+//	int findRoot(const T& x) {
+//		int parent = _indexMap[x]; //默认当前结点就是根结点
+//		while (_ufs[parent] >= 0) { //当前结点值不是负数则继续向上找
+//			parent = _ufs[parent];
+//		}
+//		return parent; //返回根结点
+//	}
+//	//合并两个元素所在的集合
+//	bool unionSet(const T& x1, const T& x2) {
+//		int parent1 = findRoot(x1), parent2 = findRoot(x2); //分别找到两个元素所在集合的根结点
+//		if (parent1 == parent2) //两个结点已经位于同一集合
+//			return false;
+//		if (_ufs[parent1] > _ufs[parent2]) //让parent1标记大集合根结点，parent2标记小集合根结点
+//			swap(parent1, parent2);
+//		//将小集合合并到大集合上
+//		_ufs[parent1] += _ufs[parent2]; //将小集合根结点的值累加到大集合的根结点上
+//		_ufs[parent2] = parent1; //将小集合根结点的值改为大集合根结点的编号
+//		return true;
+//	}
+//	//获取并查集中集合的个数
+//	int getNum() {
+//		int count = 0; //统计根结点的个数
+//		for (const int& val : _ufs) {
+//			if (val < 0) //元素值为负数则为根结点
+//				count++;
+//		}
+//		return count; //返回根结点的个数
+//	}
+//	void print() {
+//		for (const auto& e : _ufs)
+//			cout << e << " ";
+//		cout << endl;
+//	}
+//private:
+//	vector<int> _ufs; //维护各个结点之间的关系
+//	unordered_map<T, int> _indexMap; //建立元素与数组下标之间的映射关系
+//};
 
 ////省份数量
 //class Solution {
